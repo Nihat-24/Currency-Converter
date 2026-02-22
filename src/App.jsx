5f5760354f6df9d7ec366e834e66c948eba4a509
Nihat-24 <nihatagayew@gmail.com>
2026-02-22 05:51:33 +0400

    feat(app): add App component and global styles

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import Curency from './components/Curency.jsx'
+
+function App() {
+
+
+
+  return (
+    <>
+      <Curency />
+
+    </>
+  )
+}
+
+export default App
